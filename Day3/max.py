6de37c31c2fbbfe0eb18b9939a155ef5a542d832
smsowmya188-sm <smsowmya188@gmail.com>
2026-08-06 12:04:34 +0530

    task

--- Day3/max.py
+++
@@ -1,9 +0,0 @@
-def find_max(numbers):
-  if len(numbers) == 1:
-    return numbers[0]
-  else:
-    max_of_rest = find_max(numbers[1:])
-    return numbers[0] if numbers[0] > max_of_rest else max_of_rest
-
-my_list = [84,2,3,5,8,41]
-print(find_max(my_list))
--- Day3/program1.py
+++
@@ -1,7 +0,0 @@
-def my_function(name = "friend"):
-  print("Hello", name)
-
-my_function("Emil")
-my_function("Tobias")
-my_function()
-my_function("Linus")
